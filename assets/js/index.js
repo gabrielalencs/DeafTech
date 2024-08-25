28cb6599ed88c680d68243d89f9180bf65038fa3
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-08-25 14:07:02 -0300

    inicia header da página

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// const containerMenuMobile = document.querySelector('.header__content');
+// const buttonMenuMobile = document.querySelector('.header__btn-mobile');
+
+
+// buttonMenuMobile.addEventListener('change', () => {
+
+//     containerMenuMobile.classList.toggle('menu-active');
+
+// });
