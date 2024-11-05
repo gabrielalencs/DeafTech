5b103126211922923c96a1b96c09b0e9644cfcc4
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-11-05 19:25:31 -0300

    lógica de abrir e fechar tópicos dropdown

--- assets/js/materials.js
+++ assets/js/materials.js
@@ -1,8 +1,26 @@
-// header
+// Header
 
-import { toggleMenuMobileMaterials , clearMenuMobileMaterials } from "./modules/header/toggleMenu.js";
+import { toggleMenuMobileMaterials, clearMenuMobileMaterials } from "./modules/header/toggleMenu.js";
 
 const buttonMenuMobile = document.querySelector('.header-materials__btn-mobile');
 
 buttonMenuMobile.addEventListener('change', toggleMenuMobileMaterials);
 window.addEventListener('resize', clearMenuMobileMaterials);
+
+
+// Aside - Topics
+
+const buttonsTopicsList = document.querySelectorAll('.container-levels .list-item');
+
+buttonsTopicsList.forEach(topicList => {
+    topicList.addEventListener('click', function () {
+        const subtopicsList = this.querySelector('.subtopics__list');
+        const triangle = this.parentNode.querySelector('.bi-caret-right-fill');
+
+        
+        if (subtopicsList) {
+            triangle.classList.toggle('triangle-rotate');
+            subtopicsList.classList.toggle('show-subtopics');
+        }
+    });
+});
