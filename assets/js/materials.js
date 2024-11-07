526b795c2669d9096168137a6626edeae9bd866b
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-11-07 18:55:46 -0300

    tópico de abecedário

--- assets/js/materials.js
+++ assets/js/materials.js
@@ -11,16 +11,29 @@ window.addEventListener('resize', clearMenuMobileMaterials);
 // Aside - Topics
 
 const buttonsTopicsList = document.querySelectorAll('.container-levels .list-item');
+const subtopicLinks = document.querySelectorAll('.container-levels .subtopics__list a');
 
 buttonsTopicsList.forEach(topicList => {
-    topicList.addEventListener('click', function () {
+    topicList.addEventListener('click', function (event) {
         const subtopicsList = this.querySelector('.subtopics__list');
         const triangle = this.parentNode.querySelector('.bi-caret-right-fill');
-
         
+        if (!event.target.classList.contains('list-item')) return
+        
+
         if (subtopicsList) {
             triangle.classList.toggle('triangle-rotate');
             subtopicsList.classList.toggle('show-subtopics');
         }
+    });
+});
+
+subtopicLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        subtopicLinks.forEach(item => {
+            item.classList.remove('active-topic');
+        });
+
+        link.classList.add('active-topic');
     });
 });
