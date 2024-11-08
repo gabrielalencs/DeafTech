eb46134c19f32e701503285196da3d26534b87b0
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-11-07 22:19:39 -0300

    tópico perguntas comuns

--- assets/js/materials.js
+++ assets/js/materials.js
@@ -17,9 +17,9 @@ buttonsTopicsList.forEach(topicList => {
     topicList.addEventListener('click', function (event) {
         const subtopicsList = this.querySelector('.subtopics__list');
         const triangle = this.parentNode.querySelector('.bi-caret-right-fill');
-        
+
         if (!event.target.classList.contains('list-item')) return
-        
+
 
         if (subtopicsList) {
             triangle.classList.toggle('triangle-rotate');
@@ -37,3 +37,28 @@ subtopicLinks.forEach(link => {
         link.classList.add('active-topic');
     });
 });
+
+
+// Aside - Content
+
+const containerMaterial = document.querySelector('.container-material');
+
+async function getMaterials(path) {
+    const response = await fetch(path);
+    const jsonData = await response.json();
+    
+    let contentHTML = jsonData.material;
+    
+    containerMaterial.innerHTML = contentHTML;
+}
+
+// Beginner Topics
+
+const topicAlphabet = document.getElementById('topic-alphabet');
+topicAlphabet.addEventListener('click', () => getMaterials('./assets/data/topicos-iniciantes/alfabeto.json'));
+
+
+// Intermediate Topics
+
+const topicCommonQuestions = document.getElementById('topic-common-questions');
+topicCommonQuestions.addEventListener('click', () => getMaterials('./assets/data/topicos-intermediarios/perguntas-comuns.json'));
