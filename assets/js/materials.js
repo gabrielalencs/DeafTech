ccc91ae60a5a56424ed814b0d0f301a53cb82fcf
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-11-09 19:15:21 -0300

    adicionando topicos

--- assets/js/materials.js
+++ assets/js/materials.js
@@ -15,10 +15,21 @@ const subtopicLinks = document.querySelectorAll('.container-levels .subtopics__l
 
 buttonsTopicsList.forEach(topicList => {
     topicList.addEventListener('click', function (event) {
-        const subtopicsList = this.querySelector('.subtopics__list');
-        const triangle = this.parentNode.querySelector('.bi-caret-right-fill');
+        if (!event.target.classList.contains('list-item')) return;
 
-        if (!event.target.classList.contains('list-item')) return
+        const subtopicsList = this.querySelector('.subtopics__list');
+        const triangle = this.querySelector('.bi-caret-right-fill');
+
+
+        buttonsTopicsList.forEach(item => {
+            const otherSubtopics = item.querySelector('.subtopics__list');
+            const otherTriangle = item.querySelector('.bi-caret-right-fill');
+
+            if (otherSubtopics !== subtopicsList) {
+                otherSubtopics?.classList.remove('show-subtopics');
+                otherTriangle?.classList.remove('triangle-rotate');
+            }
+        });
 
 
         if (subtopicsList) {
@@ -45,19 +56,29 @@ const containerMaterial = document.querySelector('.container-material');
 
 let materialsLoaded = {
     'alfabeto': false,
-    'perguntas-comuns': false
+    'perguntas-comuns': false,
+    'numeros-e-meses': false,
+    'comprimentos-e-saudações': false,
+    'familia-e-relações': false,
 };
+let lastTopic = null;
+
+const loadingContainer = document.querySelector('.loading');
 
 async function getMaterials(path, key) {
-    if (materialsLoaded[key]) return;  
-    
+    if (materialsLoaded[key] && lastTopic === key) return;
+    loadingContainer.style.display = 'true';
+
     const response = await fetch(path);
     const jsonData = await response.json();
 
     materialsLoaded[key] = true;
-    
+    lastTopic = key;
+
     let contentHTML = jsonData.material;
     containerMaterial.innerHTML = contentHTML;
+
+    loadingContainer.style.display = 'none';
 }
 
 // Beginner Topics
@@ -65,8 +86,17 @@ async function getMaterials(path, key) {
 const topicAlphabet = document.getElementById('topic-alphabet');
 topicAlphabet.addEventListener('click', () => getMaterials('./assets/data/topicos-iniciantes/alfabeto.json', 'alfabeto'));
 
+const topicNumbers = document.getElementById('topic-numbers');
+topicNumbers.addEventListener('click', () => getMaterials('./assets/data/topicos-iniciantes/numeros-e-meses.json', 'numeros-e-meses'));
+
+const topicGreetings = document.getElementById('topic-greetings');
+topicGreetings.addEventListener('click', () => getMaterials('./assets/data/topicos-iniciantes/saudacoes.json', 'comprimentos-e-saudações'));
+
 
 // Intermediate Topics
 
 const topicCommonQuestions = document.getElementById('topic-common-questions');
 topicCommonQuestions.addEventListener('click', () => getMaterials('./assets/data/topicos-intermediarios/perguntas-comuns.json', 'perguntas-comuns'));
+
+const topicRelations = document.getElementById('topic-relations');
+topicRelations.addEventListener('click', () => getMaterials('./assets/data/topicos-intermediarios/familia-relacoes-pessoais.json', 'familia-e-relações'));
