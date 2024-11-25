c317f6d76005dece77a7f82f21b228dfe9a785ca
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-11-25 17:48:19 -0300

    implementa tela de login

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,15 @@ const buttonMenuMobile = document.querySelector('.header__btn-mobile');
 
 buttonMenuMobile.addEventListener('change', toggleMenuMobile);
 window.addEventListener('resize', clearMenuMobile);
+
+const buttonLoginUser = document.querySelector('.header__btn-login');
+const containerLogin = document.querySelector('.container-login');
+const buttonCloseLogin = document.querySelector('.close-btn-login');
+
+buttonLoginUser.addEventListener('click', () => {
+    containerLogin.classList.remove('hidden-login');
+});
+
+buttonCloseLogin.addEventListener('click', () => {
+    containerLogin.classList.add('hidden-login');
+});
